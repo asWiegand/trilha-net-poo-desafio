a241f6dc453f60c0ed6c9ab09cc556518f51234d
Willian Wiegand <willianwiegand21@gmail.com>
2025-06-18 12:41:34 -0300

    Desafio Completo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("123456", "Nokia 3310", "111111111111111", 16);
+nokia.Ligar();
+nokia.InstalarAplicativo("Snake");
+
+Console.WriteLine();
+
+Iphone iphone = new Iphone("654321", "iPhone 14", "222222222222222", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
